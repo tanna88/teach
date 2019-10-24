25fb75e401d3338130750ee85e5a502b716500dc
tannauit <tannguyenanh@gmail.com>
2019-10-24 21:49:20 +0700

    Update layout

--- bai2/js/main.js
+++ bai2/js/main.js
@@ -2,29 +2,46 @@ var ket_qua = ""
 
 function bam1() {
   ket_qua = ket_qua + "1"
+  hienPhepTinh();
 }
 
 function bam2() {
   ket_qua += "2"
+  hienPhepTinh();
 }
 
 function bam3() {
   ket_qua += "3"
+  hienPhepTinh();
 }
 
 function bam4() {
   ket_qua += "4"
+  hienPhepTinh();
 }
 
 function cong() {
   ket_qua += "+"
+  hienPhepTinh();
 }
 
 function tru() {
   ket_qua += "-"
+  hienPhepTinh();
 }
 
 function bang() {
-  alert(eval(ket_qua));
+  var inputKetQua = document.getElementById("ket_qua");
+  inputKetQua.value = eval(ket_qua);
   ket_qua = "";
+}
+
+function reset() {
+  ket_qua = "";
+  hienPhepTinh();
+}
+
+function hienPhepTinh() {
+  var inputPhepTinh = document.getElementById("phep_tinh");
+  inputPhepTinh.value = ket_qua;
 }
