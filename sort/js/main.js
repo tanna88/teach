cd7dc3c32f4e809898fa508e7b7f890faf423824
tannauit <tannguyenanh@gmail.com>
2019-12-03 17:51:37 +0700

    Ve debug

--- sort/js/main.js
+++ sort/js/main.js
@@ -24,24 +24,54 @@ function randomArray() {
 
 function redraw() {
   drawOnMe.fillStyle = 'rgba(0, 0, 0, 1)';
-  drawOnMe.fillRect(0, 0, 1000, 100);
+  drawOnMe.fillRect(0, 100, 1000, 100);
   drawOnMe.fillStyle = 'rgba(255, 255, 255, 1)';
-  drawOnMe.fillRect(10, 10, 980, 80);
+  drawOnMe.fillRect(10, 110, 980, 80);
 
   drawOnMe.fillStyle = 'rgba(0, 0, 0, 1)';
 
   // Ve day so
   for ( var i = 0; i < array.length; i++) {
-    drawOnMe.fillText(array[i],i * 30 + 20, 50);
+    drawItem(i);
   }
+}
+
+function drawItem(index, color) {
+  drawOnMe.fillStyle = color || 'rgba(0, 0, 0, 1)';
+  drawOnMe.fillText(array[index], index * 30 + 20, 150);
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
   run();
 })
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function drawAwait() {
+  drawOnMe.fillText("Cho xi, Dung bam nua. Neu thay sai roi thi refresh", 20, 20);
+}
+
+function drawDone() {
+  drawOnMe.fillText("Xong roi", 20, 70);
+}
+
 // You code below here
 
-function sort() {
-
+async function sort() {
+  drawAwait();
+  redraw();
+  drawItem(0, 'rgba(255, 0, 0, 1)');
+  await sleep(1000);
+  redraw();
+  drawItem(1, 'rgba(255, 0, 0, 1)');
+  await sleep(1000);
+  redraw();
+  drawItem(2, 'rgba(255, 0, 0, 1)');
+  await sleep(1000);
+  redraw();
+  drawItem(3, 'rgba(255, 0, 0, 1)');
+  await sleep(1000);
+  drawDone();
 }
