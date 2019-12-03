efec8557d6077b1013f9b95b581e33ab66a52811
tannauit <tannguyenanh@gmail.com>
2019-12-03 22:17:27 +0700

    Swap

---
+++ swap/js/main.js
@@ -0,0 +1,18 @@
+var a = 100;
+var b = 200;
+
+function swap() {
+  console.log("Truoc khi hoan doi");
+  console.log("a = " + a );
+  console.log("b = " + b );
+  // hoan doi gia tri
+  //
+  // code o day
+  //
+
+  // Sau khi hoan doi ta co gia tri
+  console.log("Sau khi hoan doi");
+  console.log("a = " + a );
+  console.log("b = " + b );
+}
+
